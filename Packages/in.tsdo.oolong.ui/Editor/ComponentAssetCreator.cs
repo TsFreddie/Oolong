d1a03ba954322ba6d94c9bcb7457f09504a5a0b6
Freddie <whatis@tsdo.in>
2023-09-14 23:34:26 +0800

    Script creators

---
+++ Packages/in.tsdo.oolong.ui/Editor/ComponentAssetCreator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UnityEditor;
+
+namespace TSF.Oolong.UI.Editor
+{
+    public static class ScriptAssetCreator
+    {
+        public const string TemplateGuid = "33d4d3565711766418b478d27d910417";
+
+        [MenuItem("Assets/Create/Mithril Component", priority = -101)]
+        public static void CreateTypeScript()
+        {
+            try
+            {
+                var template = AssetDatabase.GUIDToAssetPath(TemplateGuid);
+                ProjectWindowUtil.CreateScriptAssetFromTemplateFile(template, "NewComponent.tsx");
+            }
+            catch (Exception e)
+            {
+                // ignored
+            }
+        }
+    }
+}
---
+++ Packages/in.tsdo.oolong/Editor/ScriptAssetCreator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UnityEditor;
+
+namespace TSF.Oolong.Editor
+{
+    public static class ScriptAssetCreator
+    {
+        public const string TemplateGuid = "f5aed47e87ea7a841b4e06313404100f";
+
+        [MenuItem("Assets/Create/TypeScript", priority = -100)]
+        public static void CreateTypeScript()
+        {
+            try
+            {
+                var template = AssetDatabase.GUIDToAssetPath(TemplateGuid);
+                ProjectWindowUtil.CreateScriptAssetFromTemplateFile(template, "NewTypeScript.ts");
+            }
+            catch (Exception e)
+            {
+                // ignored
+            }
+        }
+    }
+}
