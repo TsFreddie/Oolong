ed4ef3c84268a9288478e4f1d561019bb8c19134
Freddie <whatis@tsdo.in>
2023-11-05 07:59:38 +0800

    Remove unfinished features

--- Packages/in.tsdo.oolong/Runtime/WorkflowExtensions.cs
+++ Packages/in.tsdo.oolong/Runtime/WorkflowExtensions.cs
@@ -1,24 +1,19 @@
-﻿using UnityEngine;
-using UnityEngine.AddressableAssets;
-namespace TSF.Oolong
-{
-    /// <summary>
-    /// Add script management extension methods to GameObject and Component
-    /// </summary>
-    public static class WorkflowExtensions
-    {
-        public static void Test()
-        {
-            GameObject a;
-        }
-
-        public static void AddScript(this GameObject go, AssetReferenceT<TextAsset> script)
-        {
-
-        }
-
-        public static void AddScript(this GameObject go, string script)
-        {
-        }
-    }
-}
+﻿// using UnityEngine;
+// using UnityEngine.AddressableAssets;
+// namespace TSF.Oolong
+// {
+//     /// <summary>
+//     /// Add script management extension methods to GameObject and Component
+//     /// </summary>
+//     public static class WorkflowExtensions
+//     {
+//         public static void AddScript(this GameObject go, AssetReferenceT<TextAsset> script)
+//         {
+//
+//         }
+//
+//         public static void AddScript(this GameObject go, string script)
+//         {
+//         }
+//     }
+// }
