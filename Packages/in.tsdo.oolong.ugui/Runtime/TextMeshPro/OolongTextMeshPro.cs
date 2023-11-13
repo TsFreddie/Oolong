3407629e5199cfc97162e34b958b4cdff45f3f5d
TsFreddie <whatis@tsdo.in>
2023-11-13 14:07:01 +0800

    Add support for TextMeshPro 4.0.0-pre.2

--- Packages/in.tsdo.oolong.ugui/Runtime/TextMeshPro/OolongTextMeshPro.cs
+++ Packages/in.tsdo.oolong.ugui/Runtime/TextMeshPro/OolongTextMeshPro.cs
@@ -1,10 +1,62 @@
 using TMPro;
 using UnityEngine;
 
+#if UNITY_TMP_4
+using UnityEngine.TextCore.Text;
+#endif
+
 namespace TSF.Oolong.UGUI
 {
     public static class OolongTextMeshPro
     {
+#if UNITY_TMP_4
+        public static void GetFont(string fontName, string materialName, out FontAsset font, out Material material)
+        {
+            var fontHashCode = fontName == null ? 0 : TMP_TextUtilities.GetSimpleHashCode(fontName);
+            var materialHashCode = materialName == null ? 0 : TMP_TextUtilities.GetSimpleHashCode(materialName);
+
+            // Special handling for <font=default> or <font=Default>
+            if (fontHashCode == 764638571 || fontHashCode == 523367755 || fontHashCode == 0)
+            {
+                font = TMP_Settings.defaultFontAsset;
+            }
+            else
+            {
+                MaterialReferenceManager.TryGetFontAsset(fontHashCode, out font);
+                if (!font)
+                {
+                    font = Resources.Load<FontAsset>(TMP_Settings.defaultFontAssetPath + fontName);
+
+                    if (font)
+                        MaterialReferenceManager.AddFontAsset(font);
+                }
+            }
+
+            if (!font)
+            {
+                font = TMP_Settings.defaultFontAsset;
+            }
+
+            if (materialHashCode == 0)
+            {
+                material = font ? font.material : null;
+                return;
+            }
+
+            if (MaterialReferenceManager.TryGetMaterial(materialHashCode, out material))
+                return;
+
+            material = Resources.Load<Material>(TMP_Settings.defaultFontAssetPath + materialName);
+
+            if (!material)
+            {
+                material = font ? font.material : null;
+                return;
+            }
+
+            MaterialReferenceManager.AddFontMaterial(materialHashCode, material);
+        }
+#else
         public static void GetFont(string fontName, string materialName, out TMP_FontAsset font, out Material material)
         {
             var fontHashCode = fontName == null ? 0 : TMP_TextUtilities.GetSimpleHashCode(fontName);
@@ -51,5 +103,6 @@ namespace TSF.Oolong.UGUI
 
             MaterialReferenceManager.AddFontMaterial(materialHashCode, material);
         }
+#endif
     }
 }
